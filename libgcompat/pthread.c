979f3bc0d18b71a6bd29557a4af9de3eaa5c34f3
William Pitcock <nenolod@dereferenced.org>
2017-06-14 20:35:13 +0000

    pthread: add __pthread_[un]register_cancel stubs, not relevant to musl pthread implementation

--- libgcompat/pthread.c
+++ libgcompat/pthread.c
@@ -5,3 +5,11 @@ int __register_atfork(void (*prepare)(void), void (*parent)(void), void (*child)
 }
 
 int register_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) __attribute__ ((weak, alias("__register_atfork")));
+
+
+void __pthread_register_cancel(void *buf) {
+}
+
+
+void __pthread_unregister_cancel(void *buf) {
+}
