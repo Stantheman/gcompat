48621083e37dcce18e017e840d79e668b5b380de
A. Wilcox <AWilcox@Wilcox-Tech.com>
2017-10-15 08:20:29 -0500

    math: Add weak aliases for non-__ prefix is{inf,nan}{,f}

--- libgcompat/math.c
+++ libgcompat/math.c
@@ -19,3 +19,8 @@ int __isnan(double number)
 {
 	return isnan(number);
 }
+
+extern __typeof(__isnanf) isnanf __attribute__((weak, alias("__isnanf")));
+extern __typeof(__isnan) isnan __attribute__((weak, alias("__isnan")));
+extern __typeof(__isinff) isinff __attribute__((weak, alias("__isinff")));
+extern __typeof(__isinf) isinf __attribute__((weak, alias("__isinf")));
