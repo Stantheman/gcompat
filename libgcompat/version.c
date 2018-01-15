900ab571ba496f73b1e66204f8c66560afda29e6
Samuel Holland <samuel@sholland.org>
2018-01-14 23:21:32 -0600

    version: Minor improvements
    * Document functions and sort by name.
    * Explicitly include stddef.h.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- libgcompat/version.c
+++ libgcompat/version.c
@@ -1,16 +1,28 @@
+#include <stddef.h> /* NULL */
 #include <stdlib.h> /* getenv */
 
+/**
+ * Get glibc release.
+ *
+ * LSB 5.0: LSB-Core-generic/baselib-gnu-get-libc-version-1.html
+ */
+const char *gnu_get_libc_release(void)
+{
+	return "stable";
+}
+
+/**
+ * Get glibc version.
+ *
+ * LSB 5.0: LSB-Core-generic/baselib-gnu-get-libc-version-1.html
+ */
 const char *gnu_get_libc_version(void)
 {
 	char *ver = getenv("GLIBC_FAKE_VERSION");
+
 	if (ver == NULL) {
 		ver = "2.8";
 	}
 
 	return ver;
-}
-
-const char *gnu_get_libc_release(void)
-{
-	return "stable";
 }
