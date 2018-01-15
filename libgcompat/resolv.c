d3e8ee9b447b1b60619f8688abe0dd992c383701
Samuel Holland <samuel@sholland.org>
2018-01-14 22:23:43 -0600

    resolv: Clean up, avoid NULL deref
    * Make comment style consistent with the rest of the project, and remove
      outdated header name.
    * Remove "static inline" from functions (this is no longer a header).
    * Check statp for NULL in res_ninit like in res_nclose.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- libgcompat/resolv.c
+++ libgcompat/resolv.c
@@ -1,34 +1,42 @@
-/* Original author: Khem Raj <raj.khem@gmail.com> */
-/***************************************************************************
- * resolv_compat.h
+/*
+ * Original author: Khem Raj <raj.khem@gmail.com>
  *
  * Mimick GLIBC's res_ninit() and res_nclose() for musl libc
  * Note: res_init() is actually deprecated according to
  * http://docs.oracle.com/cd/E36784_01/html/E36875/res-nclose-3resolv.html
- **************************************************************************/
+ */
+
 #include <resolv.h> /* res_state */
+#include <stddef.h> /* NULL */
 #include <string.h> /* memcpy, memset */
 
 #include "alias.h" /* weak_alias */
 
-static inline int __res_ninit(res_state statp)
+int __res_ninit(res_state statp)
 {
-	int rc = res_init();
+	int rc;
+
+	if (statp == NULL) {
+		return -1;
+	}
+	rc = res_init();
 	if (statp != &_res) {
 		memcpy(statp, &_res, sizeof(*statp));
 	}
+
 	return rc;
 }
 weak_alias(__res_ninit, res_ninit);
 
-static inline int __res_nclose(res_state statp)
+int __res_nclose(res_state statp)
 {
-	if (!statp) {
+	if (statp == NULL) {
 		return -1;
 	}
 	if (statp != &_res) {
 		memset(statp, 0, sizeof(*statp));
 	}
+
 	return 0;
 }
 weak_alias(__res_nclose, res_nclose);
